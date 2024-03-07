d303a14e97fbfa120e03a978a6c93804c700bcfe
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-03-08 01:49:25 +0200

    add profiles

--- models/userModel.js
+++ models/userModel.js
@@ -6,61 +6,65 @@ const userSchema = mongoose.Schema(
         type: String,
         required: true,
       },
-      profession: {
+      email: {
         type: String,
-        // required: true,
+        required: true,
       },
-      location: {
-        type: String,
-        // required: true,
-      },
-      phone: {
-        type: String,
-        // required: true,
-      },
-      rating: {
-        type: Number,
-        default: 0,
-      },
-      ratedUsers: [
-        {
-          userId: { type: mongoose.Schema.Types.ObjectId},
-          // user: { type: String},
-          userRating: { type: Number, default: 0 }
-        }
-      ],
-      comments: [
-        {
-          commenter: {
-            type: String,
-            required: true,
-          },
-          text: {
-            type: String,
-            required: true,
-          },
-          color: {
-            type: String,
-            required: true,
-          },
-          createdAt: {
-            type: Date,
-            default: Date.now,
-          },
-        }
-      ],
-      color: {
-        type: String,
-        // required: true,
-      },
-        email: {
+      password: {
           type: String,
           required: true,
         },
-        password: {
+        color: {
+          type: String,
+          required: true,
+        },
+      profiles: [
+        {
+          profession: {
             type: String,
             required: true,
           },
+          location: {
+            type: String,
+            required: true,
+          },
+          phone: {
+            type: String,
+            required: true,
+          },
+          rating: {
+            type: Number,
+            default: 0,
+          },
+          ratedUsers: [
+            {
+              userId: { type: mongoose.Schema.Types.ObjectId},
+              // user: { type: String},
+              userRating: { type: Number, default: 0 }
+            }
+          ],
+          comments: [
+            {
+              commenter: {
+                type: String,
+                required: true,
+              },
+              text: {
+                type: String,
+                required: true,
+              },
+              color: {
+                type: String,
+                required: true,
+              },
+              createdAt: {
+                type: Date,
+                default: Date.now,
+              },
+            }
+          ],
+        }
+      ],    
     }
     );
 export const User = mongoose.model('User', userSchema);
